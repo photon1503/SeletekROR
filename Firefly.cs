c1f17c5964fb94534572b41c6c0be1cd164a2148
Gerald Hitz <gerald.hitz@omv.com>
2023-12-20 16:08:09 +0100

    .

--- Firefly.cs
+++ Firefly.cs
@@ -37,21 +37,111 @@ namespace ASCOM.LocalServer.Server
 
         private static void ActivateRelais()
         {
+            Console.WriteLine("Activating relais");                
             firefly.RelayChange(seletekRelayNo);
+        }
+
+        public static void checkMovementOpen()
+        {
+            console.WriteLine("Checking movement open");
+            DateTime startTime = DateTime.Now;
+
+            while (isRoofClosed){
+                if (DateTime.Now.Subtract(startTime).TotalSeconds > 10) {
+                    Console.WriteLine("Roof is not opening");
+                    ActivateRelais();
+                }
+            }
+            console.WriteLine("Roof is opening");
+        }
+
+     public static void checkMovementClose()
+        {
+            console.WriteLine("Checking movement closing");
+            DateTime startTime = DateTime.Now;
+
+            while (isRoofOpen){
+                if (DateTime.Now.Subtract(startTime).TotalSeconds > 10) {
+                    Console.WriteLine("Roof is not closing");
+                    ActivateRelais();
+                }
+            }
+            console.WriteLine("Roof is closing");
+        }
+
+        public static void Stop()
+        {
+            ActivateRelais();
+        }
+
+        public static void Retry()
+        {
+            Console.WriteLine("Retrying to move roof");
+            ActivateRelais();         
+            Sleep(1000);
+            ActivateRelais();         
         }
 
         public static void Open()
         {
+            Console.WriteLine("Opening roof");
             if (isRoofOpen) {
+                Console.WriteLine("Roof is already open");
                 return;
             }
 
-            while (true)
-            {
+            DateTime startTime = DateTime.Now;
+            int retries = 0;
+            while (!isRoofOpen && retries < 12)
+            {                    
                 ActivateRelais();
-                // check if isRoofClosed Sensor becomes false within a few seconds
+                checkMovementOpen();                 
 
+                if (DateTime.Now.Subtract(startTime).TotalSeconds > 30) {
+                    Console.WriteLine("Roof is not opening");   
+                    Retry();   
+                    retries++;           
+                     startTime = DateTime.Now;
+                }
+                Sleep(1000);
             }
+            Console.WriteLine("Roof is open");                
+        }
+
+        public static void Close()
+        {
+            Console.WriteLine("Closing roof");
+            if (isRoofClosed) {
+                Console.WriteLine("Roof is already closed");
+                return;
+            }
+
+            DateTime startTime = DateTime.Now;
+            int retries = 0;
+            while (!isRoofClosed && retries < 12)
+            {                    
+                ActivateRelais();
+                checkMovementClose();                 
+
+                if (DateTime.Now.Subtract(startTime).TotalSeconds > 30) {
+                    Console.WriteLine("Roof is not closing");   
+                    Retry();   
+                    retries++;      
+                     startTime = DateTime.Now;     
+                }
+                Sleep(1000);
+            }
+            console.WriteLine("Roof is closed");
+        }
+    }
+
+    //main
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Firefly.Open();
+            Firefly.Close();
         }
     }
 }
